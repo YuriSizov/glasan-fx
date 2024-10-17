0c2e4ef080847ebb639242f9459070719153432e
Yuri Sizov <yuris@humnom.net>
2024-10-17 23:03:43 +0200

    Add support for icons to the glow button
    Also adjust it to allow for smaller sizes. This
    in turn means all existing buttons need to be
    adjusted to give them better min sizes. Which
    this commit also does, of course.

--- gui/components/GlowButton.gd
+++ gui/components/GlowButton.gd
@@ -29,11 +29,14 @@ var _toggle_tweener: Tween = null
 
 @onready var _panel: Panel = %Panel
 @onready var _label: Label = %Label
+@onready var _icon: TextureRect = %Icon
 
 
 func _ready() -> void:
 	_update_text()
+	_update_icon()
 	_update_label_color()
+	_update_icon_color()
 	_setup_animated_properties()
 
 	button_down.connect(_animate_button_held.bind(true))
@@ -44,16 +47,20 @@ func _ready() -> void:
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_THEME_CHANGED:
 		_update_label_color()
+		_update_icon_color()
 		_setup_animated_properties()
 	elif what == NOTIFICATION_EDITOR_PRE_SAVE:
 		_clear_label_color()
+		_clear_icon_color()
 	elif what == NOTIFICATION_EDITOR_POST_SAVE:
 		_update_label_color()
+		_update_icon_color()
 
 
 func _process(_delta: float) -> void:
 	if Engine.is_editor_hint():
 		_update_text()
+		_update_icon()
 
 
 func _toggled(_toggled_on: bool) -> void:
@@ -87,9 +94,12 @@ func set_force_glow(value: bool) -> void:
 		return
 
 	force_glow = value
+
 	_setup_label_intensity()
 	_update_label_intensity(_label_intensity)
 	_update_label_color()
+	_update_icon_color()
+
 	_setup_panel_colors()
 	_update_panel_main_color(_panel_main_color)
 	_update_panel_back_color(_panel_back_color)
@@ -155,6 +165,7 @@ func _animate_button_toggled() -> void:
 	_toggle_tweener.tween_method(_update_label_intensity, _label_intensity, label_intensity, TOGGLE_FADE_DURATION)
 
 	_toggle_tweener.tween_callback(_update_label_color)
+	_toggle_tweener.tween_callback(_update_icon_color)
 
 
 # Shader properties.
@@ -189,6 +200,7 @@ func _update_label_intensity(value: float) -> void:
 
 	_label_intensity = value
 	(_label.material as ShaderMaterial).set_shader_parameter("intensity", _label_intensity)
+	(_icon.material as ShaderMaterial).set_shader_parameter("intensity", _label_intensity)
 
 
 func _update_shader_size() -> void:
@@ -243,3 +255,42 @@ func _update_text() -> void:
 		return
 
 	_label.text = text
+
+
+# Icon properties.
+
+func _update_icon_color() -> void:
+	if not is_node_ready():
+		return
+
+	var default_font_color := get_theme_color("font_normal_color")
+	var on_font_color := get_theme_color("font_pressed_color")
+	var off_font_color := get_theme_color("font_color")
+
+	var icon_color := default_font_color
+	if toggle_mode:
+		icon_color = on_font_color if button_pressed else off_font_color
+	elif force_glow:
+		icon_color = on_font_color
+
+	_icon.self_modulate = icon_color
+
+
+func _clear_icon_color() -> void:
+	if not is_node_ready():
+		return
+
+	_icon.self_modulate = Color.WHITE
+
+
+func _update_icon() -> void:
+	if not is_node_ready():
+		return
+
+	var style := get_theme_stylebox("normal")
+
+	_icon.texture = icon
+	_icon.offset_left = style.content_margin_left
+	_icon.offset_top = style.content_margin_top
+	_icon.offset_right = -style.content_margin_right
+	_icon.offset_bottom = -style.content_margin_bottom
