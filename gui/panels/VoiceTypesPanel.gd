9dd9b2e69ac7703ef7f573f6c74ba0d4216bfdbe
Yuri Sizov <yuris@humnom.net>
2024-10-20 15:13:18 +0200

    Fix voice type button not respecting loaded SFX

--- gui/panels/VoiceTypesPanel.gd
+++ gui/panels/VoiceTypesPanel.gd
@@ -7,6 +7,8 @@
 @tool
 class_name VoiceTypesPanel extends ContentPanel
 
+var voice_buttons := preload("res://gui/panels/voice_type_buttons.tres")
+
 @onready var _siopm_button: Button = %SiOPMButton
 @onready var _opl_button: Button = %OPLButton
 @onready var _opm_button: Button = %OPMButton
@@ -25,3 +27,13 @@ func _ready() -> void:
 		_opn_button.pressed.connect(Controller.voice_manager.change_voice_type.bind(SiONDriver.CHIP_OPN))
 		_opx_button.pressed.connect(Controller.voice_manager.change_voice_type.bind(SiONDriver.CHIP_OPX))
 		_ma3_button.pressed.connect(Controller.voice_manager.change_voice_type.bind(SiONDriver.CHIP_MA3))
+
+		Controller.voice_manager.voice_changed.connect(_handle_voice_changed)
+
+
+func _handle_voice_changed() -> void:
+	var voice := Controller.voice_manager.get_voice_params()
+	var voice_type := voice.voice.get_chip_type()
+
+	if voice_buttons.get_pressed_button().get_index() != voice_type:
+		voice_buttons.get_buttons()[voice_type].button_pressed = true
